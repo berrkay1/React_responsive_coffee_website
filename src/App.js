63f980a90514dea9e924add42581db5ae74358b8
BERKAY GÜL <berrkaygul@gmail.com>
2022-04-14 03:22:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router-dom'; 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className="header">
+        <a className='logo' href="#">
+          <img src="./images/logo.png" alt="logo" />
         </a>
+
+
+        <nav className="navbar">
+          <a href="">home</a>
+          <a href="">about</a>
+          <a href="">menu</a>
+          <a href="">products</a>
+          <a href="">review</a>
+          <a href="">contact</a>
+          <a href="">blogs</a>
+        </nav>
+
+        <div className="icons">
+          <div className="fas fa-search"></div>
+          <div className="fas fa-shopping-cart"></div>
+          <div className="fas fa-bars"></div>
+        </div>
+
+
       </header>
     </div>
   );
